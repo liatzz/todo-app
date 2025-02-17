2f0ae55e6d97dc221dabdef121d77db6cc2a4c31
liatzz <2223615.selia@smkn-2sbg.sch.id>
2025-02-17 14:00:46 +0700

    memberikan komentar

--- database/migrations/2025_01_30_031550_create_task_lists_table.php
+++ database/migrations/2025_01_30_031550_create_task_lists_table.php
@@ -7,22 +7,22 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Menjalankan migrasi untuk membuat tabel "task_lists".
      */
     public function up(): void
     {
         Schema::create('task_lists', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->unique();
-            $table->timestamps();
+            $table->id(); // Membuat kolom ID sebagai primary key dengan auto increment
+            $table->string('name')->unique(); // Kolom nama yang harus unik di setiap entri
+            $table->timestamps(); // Menambahkan kolom "created_at" dan "updated_at"
         });
     }
 
     /**
-     * Reverse the migrations.
+     * Membatalkan migrasi dengan menghapus tabel "task_lists".
      */
     public function down(): void
     {
-        Schema::dropIfExists('task_lists');
+        Schema::dropIfExists('task_lists'); // Menghapus tabel "task_lists" jika ada
     }
 };
