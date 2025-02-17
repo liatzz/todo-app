29060e22a92be8f4f40d91120a6cd0bdf27733e7
liatzz <2223615.selia@smkn-2sbg.sch.id>
2025-02-17 13:51:08 +0700

    memberikan komentar

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
+    App\Providers\AppServiceProvider::class, // Menambahkan AppServiceProvider ke dalam array, yang bertugas untuk menyediakan layanan dan pengaturan pada aplikasi
 ];
