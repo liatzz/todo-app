a77854913f4830cb5f37b95c2dbbf8e64a655135
liatzz <2223615.selia@smkn-2sbg.sch.id>
2025-02-17 13:47:46 +0700

    memberikan komentar

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,15 +4,15 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+return Application::configure(basePath: dirname(__DIR__)) // Menentukan base path aplikasi, biasanya ke direktori root proyek
+    ->withRouting( // Mengonfigurasi pengaturan routing untuk aplikasi
+        web: __DIR__.'/../routes/web.php', // Menentukan path untuk routing web
+        commands: __DIR__.'/../routes/console.php', // Menentukan path untuk routing perintah console
+        health: '/up', // Menentukan route untuk health check di '/up'
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware) { // Menyediakan callback untuk mengonfigurasi middleware
         //
     })
-    ->withExceptions(function (Exceptions $exceptions) {
+    ->withExceptions(function (Exceptions $exceptions) { // Menyediakan callback untuk menangani pengecualian
         //
-    })->create();
+    })->create(); // Membuat dan mengembalikan instance aplikasi
