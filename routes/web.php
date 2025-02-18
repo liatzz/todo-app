5606cfc61ee39e7718d1850e8ed55f0240aa57c1
liatzz <2223615.selia@smkn-2sbg.sch.id>
2025-02-19 01:38:07 +0700

    menambahkan komentar

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,20 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskListController;
 use Illuminate\Support\Facades\Route;
 
-// Membuat route untuk home
+// Route untuk halaman utama, menampilkan daftar tugas
 Route::get('/', [TaskController::class, 'index'])->name('home');
 
+// Resource controller untuk Task List (CRUD: index, create, store, show, edit, update, destroy)
 Route::resource('lists', TaskListController::class);
 
+// Resource controller untuk Task (CRUD: index, create, store, show, edit, update, destroy)
 Route::resource('tasks', TaskController::class);
 
+// Route untuk menandai task sebagai selesai
 Route::patch('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
 
+// Route untuk memindahkan task ke daftar lain
 Route::patch('/tasks/{task}/change-list', [TaskController::class, 'changeList'])->name('tasks.changeList');
 
+// Route untuk toggle status selesai/tidak selesai
 Route::patch('/tasks/{task}/toggle', [TaskController::class, 'toggleComplete'])->name('tasks.toggleComplete');
