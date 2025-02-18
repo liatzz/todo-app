6a42dbd5332674d90d170003690b0544ddb8924e
liatzz <2223615.selia@smkn-2sbg.sch.id>
2025-02-18 08:59:35 +0700

    memberikan komentar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,28 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <!-- Menentukan judul halaman, jika tidak ada variabel $title maka menggunakan 'Default Title' -->
     <title>{{ $title ?? 'Default Title' }} - {{ config('app.name') }}</title>
 
-
-    <!-- Import bootstrap CSS -->
+    <!-- Import file CSS Bootstrap dari dalam project -->
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap-icons/font/bootstrap-icons.min.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
+    <!-- Import Bootstrap versi 5.3 dari CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
-    @include('partials.navbar') <!-- Mengambil component navbar -->
+    <!-- Menyertakan navbar dari file partials/navbar.blade.php -->
+    @include('partials.navbar')
 
-    @yield('content') <!-- Render content -->
+    <!-- Bagian utama halaman yang akan diisi oleh halaman lain -->
+    @yield('content')
 
+    <!-- Menyertakan modal jika ada -->
     @include('partials.modal')
 
-    @include('partials.footer') <!-- Mengambil component footer -->
+    <!-- Menyertakan footer dari file partials/footer.blade.php -->
+    @include('partials.footer')
 
+    <!-- Import file JavaScript dari dalam project -->
     <script src="{{ asset('js/script.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script> <!-- Import bootstrap JS -->
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script> <!-- Import Bootstrap JS -->
 </body>
 
 </html>
