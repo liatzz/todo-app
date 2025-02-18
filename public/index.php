7da045c6053f2a2edccca41cbe82eb97e535784f
liatzz <2223615.selia@smkn-2sbg.sch.id>
2025-02-18 08:55:58 +0700

    memberikan komentar

--- public/index.php
+++ public/index.php
@@ -2,16 +2,17 @@
 
 use Illuminate\Http\Request;
 
+// Menentukan waktu mulai eksekusi Laravel
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// Mengecek apakah aplikasi sedang dalam mode pemeliharaan
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+    require $maintenance; // Jika file maintenance.php ada, aplikasi akan masuk mode pemeliharaan
 }
 
-// Register the Composer autoloader...
+// Memuat autoloader Composer untuk memuat dependensi Laravel
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
+// Mem-bootstrapping aplikasi Laravel dan menangani request yang masuk
 (require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+    ->handleRequest(Request::capture()); // Mengambil request HTTP dan memprosesnya
