1c108da550671065e3730272582420ab4ef0f930
liatzz <2223615.selia@smkn-2sbg.sch.id>
2025-02-19 01:17:24 +0700

    memberikan komentar

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,19 +1,22 @@
 <!-- resources/views/partials/footer.blade.php -->
 <footer style="background: linear-gradient(to right, #F7D6E0, #E0BBE4, #D4E6F1); color: #4A4A4A; padding: 30px 0; text-align: center; position: relative;">
     <div class="container">
-        <p style="font-size: 1.1rem;">&copy; {{ date('Y') }} My Application. Atas bantuan ayang chat gpt</p>
+        <p style="font-size: 1.1rem;">
+            &copy; {{ date('Y') }} My Application. Atas bantuan ayang ChatGPT <!-- Pesan unik, bisa diubah sesuai preferensi -->
+        </p>
         <p>Follow us on:
-            <a href="https://facebook.com" class="text-dark" style="text-decoration: none;"> Facebook</a> |
-            <a href="https://instagram.com" class="text-dark" style="text-decoration: none;"> Instagram</a> |
-            <a href="https://github.com/liatzz" class="text-dark" style="text-decoration: none;"> GitHub</a>
+            <a href="https://facebook.com" class="text-dark" style="text-decoration: none;" target="_blank"> Facebook</a> |
+            <a href="https://instagram.com" class="text-dark" style="text-decoration: none;" target="_blank"> Instagram</a> |
+            <a href="https://github.com/liatzz" class="text-dark" style="text-decoration: none;" target="_blank"> GitHub</a> <!-- Pastikan username benar -->
         </p>
     </div>
 
-    <div style="position: absolute; bottom: 10%; left: 10%; opacity: 0.4; font-size: 50px;">
+    <!-- Dekorasi emoji kupu-kupu untuk estetika -->
+    <div style="position: absolute; bottom: 5%; left: 10%; opacity: 0.3; font-size: 50px;">
         🦋
     </div>
     
-    <div style="position: absolute; bottom: 10%; right: 10%; opacity: 0.4; font-size: 50px;">
+    <div style="position: absolute; bottom: 5%; right: 10%; opacity: 0.3; font-size: 50px;">
         🦋
     </div>
 </footer>
