41f1c5086c190418b2966f617c2513e7a5a11847
liatzz <2223615.selia@smkn-2sbg.sch.id>
2025-02-19 01:35:15 +0700

    menambahkan komentar

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+// Menambahkan perintah Artisan kustom 'inspire'
+// Perintah ini akan menampilkan kutipan inspiratif setiap kali dijalankan
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+    $this->comment(Inspiring::quote()); // Mengambil dan menampilkan kutipan dari kelas Inspiring
+})->purpose('Display an inspiring quote') // Menjelaskan tujuan perintah saat menjalankan 'php artisan list'
+  ->hourly(); // Perintah ini dijadwalkan untuk berjalan setiap jam jika digunakan dalam scheduler
