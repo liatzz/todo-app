b448428ef30c10d643dda80fb97689054764af85
liatzz <2223615.selia@smkn-2sbg.sch.id>
2025-02-18 08:27:30 +0700

    memberikan komentar

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,20 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Method ini digunakan untuk melakukan seeding ke database.
+     * Seeding adalah proses mengisi database dengan data awal (dummy data).
      */
     public function run(): void
     {
+        // Memanggil seeder TaskListSeeder untuk mengisi tabel task_lists dengan data awal.
         $this->call(TaskListSeeder::class);
+
+        // Memanggil seeder TaskSeeder untuk mengisi tabel tasks dengan data awal.
         $this->call(TaskSeeder::class);
     }
 }
